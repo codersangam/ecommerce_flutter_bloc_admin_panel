f331674877803bc7fc21c2a766cb710e3a243726
Sangam Singh <hritriik@gmail.com>
2022-07-26 19:51:15 +0400

    added required screens

---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: VxAppBar(title: 'Home Screen'.text.make()),
+    );
+  }
+}
